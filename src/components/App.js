c540671817603c9fab2c7565ed1a9c36f1117a1c
Rocío Dols Pividal <rdolspividal@gmail.com>
2022-01-18 09:11:31 +0100

    add-html-table

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,25 @@ function App() {
 
   console.log(data);
 
+  const htmlConctact = data.map(contact => (
+    <tr>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+    </tr>
+  ));
+
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <h1>Adalabers</h1>
+      <table>
+        <tr>
+          <th>Nombre</th>
+          <th>Tutora</th>
+          <th>Especialidad</th>
+        </tr>
+        {htmlConctact}
+      </table>
     </div>
   );
 }
